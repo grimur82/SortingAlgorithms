99c2403b44e1b7fd27a74189bfac25828ce92895
grimur82 <grimur82@gmail.com>
2017-04-11 21:02:12 -0500

    Added comments to functions merge(arr) and merge(left,right)

--- MergeSort.js
+++ MergeSort.js
@@ -3,19 +3,19 @@
 */
 // Split the array down to one.
 function merge(arr){
-	if(arr.length <=1){
+	if(arr.length <=1){ // Base case. replace current stack frame with value when one or less is left.
 		return arr;
 	}
 	else{		
-		var left = merge(arr.slice(0,arr.length/2));
-		var right = merge(arr.slice((arr.length/2), arr.length));	
-		return mergeSort(left,right);
+		var left = merge(arr.slice(0,arr.length/2)); // Add to stack frame the left array divided by 2
+		var right = merge(arr.slice((arr.length/2), arr.length)); // Add to	stack frame the right array divided by 2
+		return mergeSort(left,right); // Merge variable left and right to sort in mergeSort function.
 	}
 }
 // Sort the array.
 function mergeSort(left,right){
 	var newLst = [];
-	while(left.length > 0 && right.length >0){
+	while(left.length > 0 && right.length >0){ // Compare left and right list. Push to newLst. Stop if one of them has no value.
 		if(left[0] <= right[0]){
 			newLst.push(left.shift());
 		}
@@ -23,11 +23,13 @@ function mergeSort(left,right){
 			newLst.push(right.shift());
 		}
 	}
+	// If any values in left list, add to newLst 
 	while(left.length != 0){
 		newLst.push(left.shift());
 	}
+	// If any values in right list, add to newLst
 	while(right.length !=0){
 		newLst.push(right.shift());
 	}
-	return newLst;
+	return newLst; // return sorted list.
 }
